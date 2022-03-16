cdedcaa701afaeff31c3bc46d0ea1192bf3eb1da
JSVenger <peteryar19@gmail.com>
2022-03-16 18:10:26 +0100

    langton ant  all movement error fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let antMovement;
 
-let currentPoint = 61;
+let currentPoint = 62;
 let nextPos;
 
 let childOrient = 'right';
@@ -52,13 +52,21 @@ function getAntPosInfo(pos) {
 
     if (antMovement == 'clockwise') {
         if (childOrient == 'up') {
-            nextPos = pos + 1;
+            if (pos % 11 == 0) {
+                nextPos = pos - 10;
+            } else {
+                nextPos = pos + 1;
+            }
+            console.log('next', nextPos);
             childOrient = 'right';
         } else if (childOrient == 'right') {
             nextPos = pos + 11;
             childOrient = 'down';
         } else if (childOrient == 'down') {
             nextPos = pos - 1;
+            if (nextPos % 11 == 0) {
+                nextPos = nextPos + 11;
+            }
             childOrient = 'left';
         } else {
             nextPos = pos - 11;
@@ -67,12 +75,19 @@ function getAntPosInfo(pos) {
     } else {
         if (childOrient == 'up') {
             nextPos = pos - 1;
+            if (nextPos % 11 == 0) {
+                nextPos = nextPos + 11;
+            }
             childOrient = 'left';
         } else if (childOrient == 'right') {
             nextPos = pos - 11;
             childOrient = 'up';
         } else if (childOrient == 'down') {
-            nextPos = pos + 1;
+            if (pos % 11 == 0) {
+                nextPos = pos - 10;
+            } else {
+                nextPos = pos + 1;
+            }
             childOrient = 'right';
         } else {
             nextPos = pos + 11;
@@ -88,15 +103,17 @@ function getAntPosInfo(pos) {
 }
 
 function moveAnt(pos) {
+    //get the current element ant is currently at
     const el = document.querySelector(`.no${currentPoint}`);
 
-    el.innerHTML = '';
+    el.innerHTML = '';  //remove ant from the element
 
+    //next element ant will be moving to
     const newEl = document.querySelector(`.no${pos}`);
 
+    //creates ant el
     let ant = document.createElement('img');
     ant.setAttribute('src', './assets/Red_ant.svg');
-
     ant.classList.add(childOrient);
 
     newEl.appendChild(ant);
@@ -120,18 +137,19 @@ function runLangtonAnt(speed) {
         moveAnt(nextPos);
     }, speed);
 }
+
 inputEl.addEventListener('keyup', function (e) {
-    speed = Number(e.target.value) ;
+    speed = Number(e.target.value);
 });
 
 let runningAnt;
 
 startButton.onclick = function () {
-    if(runningAnt){
+    if (runningAnt) {
         alert('stop running ant first');
         return;
     }
-    if(!speed){
+    if (!speed) {
         alert('please enter speed');
         return;
     }
@@ -145,3 +163,4 @@ stopButton.onclick = function () {
 
 
 // langtonAnt();
+
--- src/index.test.js
+++ src/index.test.js
@@ -4,26 +4,41 @@ import { JSDOM } from 'jsdom';
 import fs from 'fs';
 import path from 'path';
 
+// import { setupPlayground } from './index';
+
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
 let dom;
 let body;
 
 describe('index.html', () => {
-  beforeEach(() => {
-    // Constructing a new JSDOM with this option is the key
-    // to getting the code in the script tag to execute.
-    // This is indeed dangerous and should only be done with trusted content.
-    // https://github.com/jsdom/jsdom#executing-scripts
-    dom = new JSDOM(html, { runScripts: 'dangerously' });
-    body = dom.window.document.body;
-  });
+    beforeEach(() => {
+        // Constructing a new JSDOM with this option is the key
+        // to getting the code in the script tag to execute.
+        // This is indeed dangerous and should only be done with trusted content.
+        dom = new JSDOM(html, { runScripts: 'dangerously' });
+        body = dom.window.document.body;
+    });
 
-  it('should render two divs element', () => {
-    expect(body.querySelector('.header')).not.toBeNull();
-    expect(body.querySelector('.langton-box-con')).not.toBeNull();
-  });
-  it('should render an element with id langton-box', ()=>{
-     expect(body.querySelector('#langton-box')).not.toBeNull();
-  });
+    it('should render two divs element', () => {
+        expect(body.querySelector('.header')).not.toBeNull();
+        expect(body.querySelector('.langton-box-con')).not.toBeNull();
+    });
+    it('should render an element with id langton-box', () => {
+        console.log('langton-box', body.querySelector('#langton-box').children);
+        expect(body.querySelector('#langton-box')).not.toBeNull();
+    });
+
+    // describe('index.js', () => {
+    //     beforeEach(() => {
+    //         dom = new JSDOM(html, { runScripts: 'dangerously' });
+    //         body = dom.window.document.body;
+    //     });
+
+    //     // setupPlayground();
+    //     // it('setPlayground shoudld append 121 element as children langton-box element', () => {
+    //     //     console.log('langton-box', body.querySelector('#langton-box'));
+    //     // });
+    // });
+
 });
